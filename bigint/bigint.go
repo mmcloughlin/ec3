487c40e6e29fb205e467ce7636ed62dbae0bd378
Michael McLoughlin <mmcloughlin@gmail.com>
2019-06-25 01:51:31 -0700

    addchain: more progress on bos-coster

--- bigint/bigint.go
+++ bigint/bigint.go
@@ -27,3 +27,35 @@ func Sort(xs []*big.Int) {
 		return xs[i].Cmp(xs[j]) < 0
 	})
 }
+
+// InsertSortedUnique inserts an integer into a slice of sorted distinct
+// integers.
+func InsertSortedUnique(xs []*big.Int, x *big.Int) []*big.Int {
+	return MergeUnique([]*big.Int{x}, xs)
+}
+
+// MergeUnique merges two slices of sorted distinct integers. Elements in both
+// slices are deduplicated.
+func MergeUnique(xs, ys []*big.Int) []*big.Int {
+	r := make([]*big.Int, 0, len(xs)+len(ys))
+
+	for len(xs) > 0 && len(ys) > 0 {
+		switch xs[0].Cmp(ys[0]) {
+		case -1:
+			r = append(r, xs[0])
+			xs = xs[1:]
+		case 0:
+			r = append(r, xs[0])
+			xs = xs[1:]
+			ys = ys[1:]
+		case 1:
+			r = append(r, ys[0])
+			ys = ys[1:]
+		}
+	}
+
+	r = append(r, xs...)
+	r = append(r, ys...)
+
+	return r
+}
