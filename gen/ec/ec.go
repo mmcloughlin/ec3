9ed05eb2ba5068ee43b9402d6cbd2113dbedfd75
Michael McLoughlin <mmcloughlin@gmail.com>
2019-08-03 18:51:24 -0700

    wip

--- gen/ec/ec.go
+++ gen/ec/ec.go
@@ -2,6 +2,7 @@ package ec
 
 import (
 	"fmt"
+	"go/types"
 	"strings"
 
 	"golang.org/x/xerrors"
@@ -15,10 +16,139 @@ import (
 	"github.com/mmcloughlin/ec3/internal/gocode"
 )
 
+type Type struct {
+	Name        string
+	ElementType types.Type
+	Coordinates []string
+}
+
+func (t Type) Print(p *gocode.Generator) {
+	p.Linef("type %s struct {", t.Name)
+	for _, c := range t.Coordinates {
+		p.Linef("\t%s %s", c, t.ElementType)
+	}
+	p.Linef("}")
+}
+
+type Parameter struct {
+	Name string
+	Type Type
+}
+
 type Function struct {
-	Name       string
-	Parameters []string
-	Formula    *efd.Formula
+	Name     string
+	Receiver *Parameter
+	Params   []*Parameter
+	Results  []*Parameter
+	Formula  *efd.Formula
+}
+
+// PrimitiveProgram lower the formula program to a primitive.
+func (f Function) PrimitiveProgram() (*ast.Program, error) {
+	original := f.Formula.Program
+	if original == nil {
+		return nil, xerrors.Errorf("function %s: missing op3 program", f.Name)
+	}
+	return op3.Lower(original)
+}
+
+// Outputs returns all output parameters. The receiver is considered an output, if present.
+func (f Function) Outputs() []*Parameter {
+	outputs := []*Parameter{}
+	if f.Receiver != nil {
+		outputs = append(outputs, f.Receiver)
+	}
+	outputs = append(outputs, f.Results...)
+	return outputs
+}
+
+// Parameters returns all input and output parameters.
+func (f Function) Parameters() []*Parameter {
+	params := append([]*Parameter{}, f.Params...)
+	return append(params, f.Outputs()...)
+}
+
+func (f Function) Print(p *gocode.Generator) {
+	// Setup input and outputs.
+	points := append([]string{}, fn.Parameters...)
+	points = append(points, p.Receiver())
+
+	variables := map[ast.Variable]string{}
+	n := 1
+	for _, point := range points {
+		for _, v := range p.Representation.Variables {
+			name := ast.Variable(fmt.Sprintf("%s%d", v, n))
+			code := fmt.Sprintf("&%s.%s", point, v)
+			variables[name] = code
+		}
+		n++
+	}
+
+	// Function header.
+	p.Printf("func (%s *%s) %s(%s *%s)", p.Receiver(), p.TypeName, fn.Name, strings.Join(fn.Parameters, ", "), p.TypeName)
+	p.EnterBlock()
+
+	// Set of defined names.
+	symbols := map[string]bool{}
+	symbols[p.Receiver()] = true
+	for _, param := range fn.Parameters {
+		symbols[param] = true
+	}
+
+	// Allocate temporaries.
+	p.Linef("var (")
+	for _, v := range op3.Variables(prog) {
+		if _, ok := variables[v]; ok {
+			continue
+		}
+		name := string(v)
+		if _, ok := symbols[name]; ok {
+			name += "_"
+		}
+		p.Linef("%s %s", name, p.Field.Type())
+		variables[v] = fmt.Sprintf("&%s", name)
+	}
+	p.Linef(")")
+
+	// Generate program.
+	p.NL()
+	for _, a := range prog.Assignments {
+		// TODO(mbm): ugly duplication in the switch statement below
+		switch e := a.RHS.(type) {
+		case ast.Pow:
+			if e.N != 2 {
+				p.SetError(errutil.AssertionFailure("power expected to be square"))
+				return
+			}
+			p.Linef("Sqr(%s, %s)", variables[a.LHS], variables[e.X])
+		case ast.Mul:
+			vx, okx := e.X.(ast.Variable)
+			vy, oky := e.Y.(ast.Variable)
+			if !okx || !oky {
+				p.SetError(errutil.AssertionFailure("operands should be variables"))
+			}
+			p.Linef("Mul(%s, %s, %s)", variables[a.LHS], variables[vx], variables[vy])
+		case ast.Sub:
+			vx, okx := e.X.(ast.Variable)
+			vy, oky := e.Y.(ast.Variable)
+			if !okx || !oky {
+				p.SetError(errutil.AssertionFailure("operands should be variables"))
+			}
+			p.Linef("Sub(%s, %s, %s)", variables[a.LHS], variables[vx], variables[vy])
+		case ast.Add:
+			vx, okx := e.X.(ast.Variable)
+			vy, oky := e.Y.(ast.Variable)
+			if !okx || !oky {
+				p.SetError(errutil.AssertionFailure("operands should be variables"))
+			}
+			p.Linef("Add(%s, %s, %s)", variables[a.LHS], variables[vx], variables[vy])
+		default:
+			p.SetError(errutil.UnexpectedType(e))
+			return
+		}
+	}
+
+	p.LeaveBlock()
 }
 
 type Config struct {
