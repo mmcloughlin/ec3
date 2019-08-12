3bfb0a63e0b5134e253d2d9b77ab9e8956a065e1
Michael McLoughlin <mmcloughlin@gmail.com>
2019-08-11 21:40:13 -0700

    clean up point test file

--- examples/p256/point_test.go
+++ examples/p256/point_test.go
@@ -13,20 +13,8 @@ var curve = elliptic.P256()
 
 func RandPoint() (x, y *big.Int) {
 	var k [32]byte
-	for {
-		rand.Read(k[:])
-		x, y = curve.ScalarBaseMult(k[:])
-		if x.Sign() != 0 && y.Sign() != 0 {
-			return
-		}
-	}
-}
-
-func LogInt(t *testing.T, name string, x *big.Int) {
-	t.Logf("%s = %x", name, x)
-	var e Elt
-	e.SetInt(x)
-	t.Logf("%s = %x (elt)", name, e)
+	rand.Read(k[:])
+	return curve.ScalarBaseMult(k[:])
 }
 
 func TestAffineRoundTrip(t *testing.T) {
@@ -65,22 +53,15 @@ func TestAddPoints(t *testing.T) {
 		ex, ey := curve.Add(x1, y1, x2, y2)
 		gx, gy := AddPoints(x1, y1, x2, y2)
 
-		if !EqualInt(t, "x", ex, gx) || !EqualInt(t, "y", ey, gy) {
-			LogInt(t, "x1", x1)
-			LogInt(t, "y1", y1)
-			LogInt(t, "x2", x2)
-			LogInt(t, "y2", y2)
-			t.FailNow()
-		}
+		EqualInt(t, "x", ex, gx)
+		EqualInt(t, "y", ey, gy)
 	}
 }
 
-func EqualInt(t *testing.T, name string, expect, got *big.Int) bool {
+func EqualInt(t *testing.T, name string, expect, got *big.Int) {
 	if !bigint.Equal(expect, got) {
-		t.Errorf("%s: not equal", name)
 		t.Logf("   got %x", got)
 		t.Logf("expect %x", expect)
-		return false
+		t.Fatalf("%s: not equal", name)
 	}
-	return true
 }
