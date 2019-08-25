272f24a24de131c13d36227d9c1c68c5ccfbdb55
Michael McLoughlin <mmcloughlin@gmail.com>
2019-08-25 13:52:09 -0700

    shortw templates
    Updates #63

---
+++ gen/curve/tmpl/shortw/stubs.go
@@ -0,0 +1,41 @@
+package shortw
+
+import "math/big"
+
+const (
+	ConstCanonicalName = "Curve-Name"
+	ConstPDecimal      = "39402006196394479212279040100143613805079739270465446667948293404245721771496870329047266088258938001861606973112319"
+	ConstNDecimal      = "39402006196394479212279040100143613805079739270465446667946905279627659399113263569398956308152294913554433653942643"
+	ConstBHex          = "b3312fa7e23ee7e4988e056be3f82d19181d9c6efe8141120314088f5013875ac656398d8a2ed19d2a85c8edd3ec2aef"
+	ConstGxHex         = "aa87ca22be8b05378eb1c71ef320ad746e1d3b628ba79b9859f741e082542a385502f25dbf55296c3a545e3872760ab7"
+	ConstGyHex         = "3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5f"
+	ConstBitSize       = 384
+)
+
+type Affine struct{}
+
+func NewAffine(x, y *big.Int) *Affine {
+	return &Affine{}
+}
+
+func (p *Affine) Coordinates() (X, Y *big.Int) {
+	X = new(big.Int)
+	Y = new(big.Int)
+	return
+}
+
+type Jacobian struct{}
+
+func NewFromAffine(a *Affine) *Jacobian {
+	return &Jacobian{}
+}
+
+func (p *Jacobian) Affine() *Affine {
+	return &Affine{}
+}
+
+func (p *Jacobian) Add(q, r *Jacobian) {
+}
+
+func (p *Jacobian) Double(q *Jacobian) {
+}
