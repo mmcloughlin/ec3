bcaab563e63b714dd952623f571781a3450eebe8
Michael McLoughlin <mmcloughlin@gmail.com>
2019-08-25 16:03:24 -0700

    ast replacements
    Updates #63

---
+++ internal/tmpl/transform.go
@@ -0,0 +1,93 @@
+package tmpl
+
+import (
+	"fmt"
+	"go/ast"
+	"go/token"
+
+	"golang.org/x/tools/go/ast/astutil"
+
+	"golang.org/x/xerrors"
+)
+
+type visitor struct {
+	Pre   astutil.ApplyFunc
+	Post  astutil.ApplyFunc
+	Error func() error
+}
+
+func (v visitor) Transform(n ast.Node) (ast.Node, error) {
+	node := astutil.Apply(n, v.Pre, v.Post)
+	if err := v.Error(); err != nil {
+		return nil, err
+	}
+	return node, nil
+}
+
+func SetPackageName(name string) Transform {
+	found := false
+	return visitor{
+		Post: func(c *astutil.Cursor) bool {
+			if _, ok := c.Parent().(*ast.File); ok && c.Name() == "Name" {
+				c.Replace(&ast.Ident{
+					Name: name,
+				})
+				found = true
+				return false
+			}
+			return true
+		},
+		Error: func() error {
+			if !found {
+				return xerrors.New("package declaration not found")
+			}
+			return nil
+		},
+	}
+}
+
+func replace(name string, replacement ast.Node) Transform {
+	n := 0
+	return visitor{
+		Post: func(c *astutil.Cursor) bool {
+			if i, ok := c.Node().(*ast.Ident); ok && i.Name == name {
+				c.Replace(replacement)
+				n++
+			}
+			return true
+		},
+		Error: func() error {
+			if n == 0 {
+				return xerrors.New("no replacements")
+			}
+			return nil
+		},
+	}
+}
+
+func Rename(from, to string) Transform {
+	return replace(from, &ast.Ident{Name: to})
+}
+
+func DefineLiteral(name string, kind token.Token, value string) Transform {
+	return replace(name, &ast.BasicLit{
+		Kind:  kind,
+		Value: value,
+	})
+}
+
+func DefineLiteralf(name string, kind token.Token, format string, args ...interface{}) Transform {
+	return DefineLiteral(name, kind, fmt.Sprintf(format, args...))
+}
+
+func DefineString(name, value string) Transform {
+	return DefineLiteralf(name, token.STRING, "%q", value)
+}
+
+func DefineIntDecimal(name string, value int) Transform {
+	return DefineLiteralf(name, token.INT, "%d", value)
+}
+
+func DefineIntHex(name string, value int) Transform {
+	return DefineLiteralf(name, token.INT, "%#x", value)
+}
