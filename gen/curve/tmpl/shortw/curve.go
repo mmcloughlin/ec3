f2a895f374804bc4382734235e3aea6508474b5c
Michael McLoughlin <mmcloughlin@gmail.com>
2019-09-03 18:35:24 -0700

    cleanup #67

--- gen/curve/tmpl/shortw/curve.go
+++ gen/curve/tmpl/shortw/curve.go
@@ -53,32 +53,98 @@ func (c curve) Double(x1, y1 *big.Int) (x, y *big.Int) {
 	return d.Affine().Coordinates()
 }
 
+// ScalarMult returns k*(x1,y1) where k is a number in big-endian form.
+func (c curve) ScalarMult(x1, y1 *big.Int, k []byte) (x, y *big.Int) {
+	// Implementation follows [msrecclibpaper] Algorithm 1.
+
+	// Scalar recoding window size.
+	const w = ConstW
+
+	// Convert point from affine.
+	a := NewAffine(x1, y1)
+	p := NewFromAffine(a)
+
+	// Step 1: scalar validation.
+
+	// TODO(mbm): exit if scalar is 0.
+
+	var K scalar
+	K.SetBytes(k)
+
+	// Step 5: odd = k mod 2
+	// Step 6: if odd = 0 then k = r − k
+	even := K.ConvertToOdd()
+
+	// Step 7: Recode k to (k_t, ..., k_0) using Algorithm 6.
+	digits := K.FixedWindowRecode()
+
+	// Step 4: Compute P[i] = (2i + 1)P for 0 ⩽ i < 2^{w−2}.
+	var tbl table
+	tbl.Precompute(p)
+
+	// Step 8: Q = s_t * P[(|k_t| − 1)/2]
+	var q, r Jacobian
+
+	t := len(digits) - 1
+	tbl.Lookup(&q, digits[t])
+
+	// Step 9: for i = (t − 1) to 1
+	for i := t - 1; i >= 1; i-- {
+		// Step 14: Q = 2^{w−1}Q
+		for j := 0; j < w-1; j++ {
+			q.Double(&q)
+		}
+
+		// Step 15: Q = Q + s_i * P[(|k_i| − 1)/2]
+		tbl.Lookup(&r, digits[i])
+		q.Add(&q, &r)
+	}
+
+	// Step 18: Q = 2^{w−1}Q
+	for j := 0; j < w-1; j++ {
+		q.Double(&q)
+	}
+
+	// Step 19: Q = Q ⊕ s_0 * P[(|k_0| − 1)/2]
+
+	// TODO(mbm): must be a complete addition formula
+
+	tbl.Lookup(&r, digits[0])
+	q.Add(&q, &r)
+
+	// Step 20: if odd = 0 then Q = −Q
+	q.CNeg(even)
+
+	// Step 21: Convert Q to affine coordinates (x, y).
+	return q.Affine().Coordinates()
+}
+
 // tablesize is the size of the lookup table used by ScalarMult.
 const tablesize = 1 << (ConstW - 1)
 
+// table is a lookup table used by ScalarMult.
 type table [tablesize]Jacobian
 
-// precompute a lookup table of odd multiples of P.
-func precompute(P *Jacobian) *table {
-	var t table
-	t[0].Set(P)
+// Precompute odd multiples of p.
+func (t *table) Precompute(p *Jacobian) {
+	t[0].Set(p)
 
-	var _2P Jacobian
-	_2P.Double(P)
+	var _2p Jacobian
+	_2p.Double(p)
 
 	for i := 1; i < tablesize; i++ {
-		t[i].Add(&t[i-1], &_2P)
+		t[i].Add(&t[i-1], &_2p)
 	}
-
-	return &t
 }
 
-func (t *table) lookup(P *Jacobian, digit int32) {
+// Lookup an odd multiple from the table and store it in p. The provided digit
+// may be negative, in which case p will be negated.
+func (t *table) Lookup(p *Jacobian, digit int32) {
 	idx := abs(digit) / 2
 	for i := range t {
-		P.CMov(&t[i], uint(subtle.ConstantTimeEq(int32(i), idx)))
+		p.CMov(&t[i], uint(subtle.ConstantTimeEq(int32(i), idx)))
 	}
-	P.CNeg(sign(digit))
+	p.CNeg(sign(digit))
 }
 
 // abs returns the absolute value of x.
@@ -91,123 +157,3 @@ func abs(x int32) int32 {
 func sign(x int32) uint {
 	return uint(x>>31) & 1
 }
-
-// ScalarMult returns k*(x1,y1) where k is a number in big-endian form.
-func (c curve) ScalarMult(x1, y1 *big.Int, k []byte) (x, y *big.Int) {
-	// Implementation follows [msrecclibpaper] Algorithm 1.
-
-	a := NewAffine(x1, y1)
-	P := NewFromAffine(a)
-
-	// Step 1: scalar validation.
-	// TODO(mbm): exit if scalar is 0.
-
-	// 	const omega = uint(5)
-	// 	k = c.reduceScalar(k)
-
-	var K scalar
-	K.SetBytes(k)
-
-	// Step 5: odd = k mod 2
-	// Step 6: if odd = 0 then k = r − k
-
-	// 	oddK, isEvenK := c.toOdd(k)
-
-	even := K.ConvertToOdd()
-	// log.Print(even)
-
-	// Step 7: Recode k to (kt, . . . , k0) using Algorithm 6.
-
-	//
-	// 	var scalar big.Int
-	// 	scalar.SetBytes(oddK)
-	// 	if scalar.Sign() ≡ 0 {
-	// 		return new(big.Int), new(big.Int)
-	// 	}
-	// 	L := math.SignedDigit(&scalar, omega)
-
-	digits := K.FixedWindowRecode()
-	//log.Print(digits)
-
-	// Step 4: Compute P[i] = (2i + 1)P for 0 <= i < 2^{w−2}.
-	tbl := precompute(P)
-
-	// 	TabP := newAffinePoint(Px, Py).oddMultiples(omega)
-
-	// Step 8: Q = stP[(|kt| − 1)/2]
-
-	// 	var Q, R jacobianPoint
-
-	var Q, R Jacobian
-
-	t := len(digits) - 1
-	tbl.lookup(&Q, digits[t])
-
-	// Step 9: for i = (t − 1) to 1
-	for i := t - 1; i >= 1; i-- {
-		// Step 14: Q = 2^{w−1}Q
-
-		// 		for j := uint(0); j < omega-1; j++ {
-		// 			Q.double()
-		// 		}
-
-		for j := 0; j < ConstW-1; j++ {
-			Q.Double(&Q)
-		}
-
-		// 15. Q = Q + siP[(|ki| − 1)/2]
-
-		// 		idx := absolute(L[i]) >> 1
-		// 		for j := range TabP {
-		// 			R.cmov(&TabP[j], subtle.ConstantTimeEq(int32(j), idx))
-		// 		}
-		// 		R.cneg(int(L[i]>>31) & 1)
-		// 		Q.add(&Q, &R)
-
-		tbl.lookup(&R, digits[i])
-		Q.Add(&Q, &R)
-	}
-
-	// Step 18: Q = 2^{w−1}Q
-	for j := 0; j < ConstW-1; j++ {
-		Q.Double(&Q)
-	}
-
-	// Step 19: Q = Q ⊕ s0P[(|k0| − 1)/2]
-
-	// TODO(mbm): must be a complete addition formula
-
-	tbl.lookup(&R, digits[0])
-	Q.Add(&Q, &R)
-
-	// Step 20: if odd = 0 then Q = −Q
-
-	// 	Q.cneg(isEvenK)
-
-	Q.CNeg(even)
-
-	// Step 21: Convert Q to affine coordinates (x, y).
-
-	// 	return Q.toAffine().toInt()
-
-	return Q.Affine().Coordinates()
-}
-
-// 1. if k = 0 ∨ k ≥ r then return (“error: invalid scalar”) [if: validation]
-// 2. Run point validation and compute T = 4P (for Ed) using Algorithm 2 for Eb and Algorithm 3 for Ed. If “invalid” return
-// (“error: invalid point”), else set P = T (for Ed). [if: validation]
-// Precomputation Stage:
-// 3. Fix the window width 2 ≤ w < 10 ∈ Z+.
-// Recoding Stage:
-// (r)/(w − 1)e and sj are the signs of
-// the recoded digits.
-// Evaluation Stage:
-// 10. if DBLADD = true ∧ w 6= 2 then [if: algorithm variant]
-// 11. Q = 2(w−2)Q (Use Alg.10)
-// 12. Q = 2Q + siP[(|ki| − 1)/2] (Use Alg.11)
-// 13. else
-// 14. Q = 2(w−1)Q (Use Alg.10 for Eb and Alg.14 for Ed)
-// 15. Q = Q + siP[(|ki| − 1)/2] (Use Alg.12 for Eb and Alg.15 for Ed)
-// 16. end if
-// 17. end for
-// 22. return Q.
