b3a6dd9d352be331513af2e87107c764e9729750
Michael McLoughlin <mmcloughlin@gmail.com>
2019-09-14 19:06:26 -0700

    gen/curve: test for P+(-P)
    Updates #55 #89

--- gen/curve/tmpl/shortw/curve_test.go
+++ gen/curve/tmpl/shortw/curve_test.go
@@ -27,22 +27,37 @@ func TestCurveAddRand(t *testing.T) {
 
 func TestCurveAddAsDouble(t *testing.T) {
 	for trial := 0; trial < ConstNumTrials; trial++ {
-		x1, y1 := RandPoint(t)
+		x, y := RandPoint(t)
 
-		gx, gy := cur.Add(x1, y1, x1, y1)
-		ex, ey := ref.Double(x1, y1)
+		gx, gy := cur.Add(x, y, x, y)
+		ex, ey := ref.Double(x, y)
 
 		EqualInt(t, "x", ex, gx)
 		EqualInt(t, "y", ey, gy)
 	}
 }
 
+func TestCurveAddNegative(t *testing.T) {
+	for trial := 0; trial < ConstNumTrials; trial++ {
+		x, y := RandPoint(t)
+
+		nx := new(big.Int).Set(x)
+		ny := new(big.Int).Neg(y)
+
+		gx, gy := cur.Add(x, y, nx, ny)
+		zero := new(big.Int)
+
+		EqualInt(t, "x", zero, gx)
+		EqualInt(t, "y", zero, gy)
+	}
+}
+
 func TestCurveDoubleRand(t *testing.T) {
 	for trial := 0; trial < ConstNumTrials; trial++ {
-		x1, y1 := RandPoint(t)
+		x, y := RandPoint(t)
 
-		gx, gy := ref.Double(x1, y1)
-		ex, ey := cur.Double(x1, y1)
+		gx, gy := ref.Double(x, y)
+		ex, ey := cur.Double(x, y)
 
 		EqualInt(t, "x", ex, gx)
 		EqualInt(t, "y", ey, gy)
@@ -52,10 +67,10 @@ func TestCurveDoubleRand(t *testing.T) {
 func TestCurveScalarMultRand(t *testing.T) {
 	for trial := 0; trial < ConstNumTrials; trial++ {
 		k := RandScalarNonZero(t)
-		x1, y1 := RandPoint(t)
+		x, y := RandPoint(t)
 
-		gx, gy := cur.ScalarMult(x1, y1, k.Bytes())
-		ex, ey := ref.ScalarMult(x1, y1, k.Bytes())
+		gx, gy := cur.ScalarMult(x, y, k.Bytes())
+		ex, ey := ref.ScalarMult(x, y, k.Bytes())
 
 		EqualInt(t, "x", ex, gx)
 		EqualInt(t, "y", ey, gy)
