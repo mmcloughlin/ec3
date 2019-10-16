cf644b2996d61d37e728c2f82a6697fd82d99a26
Michael McLoughlin <mmcloughlin@gmail.com>
2019-10-16 02:08:46 -0700

    internal/z3: start generating bool operations
    [ci skip]
    
    Updates #99

--- internal/z3/bool.go
+++ internal/z3/bool.go
@@ -23,16 +23,35 @@ type Bool struct {
 	ast C.Z3_ast
 }
 
-func (l *Bool) Not() *Bool {
-	return &Bool{
-		ctx: l.ctx,
-		ast: C.Z3_mk_not(l.ctx, l.ast),
-	}
-}
+//go:generate go run wrap.go -type Bool -input $GOFILE -output zbool.go
 
-func (l *Bool) Iff(r *Bool) *Bool {
-	return &Bool{
-		ctx: l.ctx,
-		ast: C.Z3_mk_iff(l.ctx, l.ast, r.ast),
-	}
-}
+// Z3_ast Z3_API 	Z3_mk_eq (Z3_context c, Z3_ast l, Z3_ast r)
+//  	Create an AST node representing l = r. More...
+//
+// Z3_ast Z3_API 	Z3_mk_distinct (Z3_context c, unsigned num_args, Z3_ast const args[])
+//  	Create an AST node representing distinct(args[0], ..., args[num_args-1]). More...
+//
+
+//wrap:doc Not returns not(x).
+//wrap:unary Not Z3_mk_not
+
+//
+// Z3_ast Z3_API 	Z3_mk_ite (Z3_context c, Z3_ast t1, Z3_ast t2, Z3_ast t3)
+//  	Create an AST node representing an if-then-else: ite(t1, t2, t3). More...
+//
+
+//wrap:doc Iff returns x iff y.
+//wrap:binary Iff Z3_mk_iff
+
+//
+// Z3_ast Z3_API 	Z3_mk_implies (Z3_context c, Z3_ast t1, Z3_ast t2)
+//  	Create an AST node representing t1 implies t2. More...
+//
+// Z3_ast Z3_API 	Z3_mk_xor (Z3_context c, Z3_ast t1, Z3_ast t2)
+//  	Create an AST node representing t1 xor t2. More...
+//
+// Z3_ast Z3_API 	Z3_mk_and (Z3_context c, unsigned num_args, Z3_ast const args[])
+//  	Create an AST node representing args[0] and ... and args[num_args-1]. More...
+//
+// Z3_ast Z3_API 	Z3_mk_or (Z3_context c, unsigned num_args, Z3_ast const args[])
+//  	Create an AST node representing args[0] or ... or args[num_args-1]. More...
