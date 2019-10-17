b41318aea8e167db294f60d2258966eb4c4f9f87
Michael McLoughlin <mmcloughlin@gmail.com>
2019-10-16 21:08:47 -0700

    internal/z3: register error handler
    Registers a fallback error handler with Z3_get_error_msg.
    
    Updates #99
    
    [ci skip]

--- internal/z3/context.go
+++ internal/z3/context.go
@@ -4,10 +4,20 @@ package z3
 #cgo LDFLAGS: -lz3
 #include <z3.h>
 #include <stdlib.h>
+
+extern void errorhandler(Z3_context c, Z3_error_code e);
 */
 import "C"
 
 import "unsafe"
+
+// errorhandler is a callback for Z3 errors. Currently panics, simply to ensure errors do not go unnoticed.
+// TODO(mbm): handle z3 errors more gracefully
+//export errorhandler
+func errorhandler(ctx C.Z3_context, code C.Z3_error_code) {
+	msg := C.Z3_get_error_msg(ctx, code)
+	panic(C.GoString(msg))
+}
 
 // Context is an environment for interacting with Z3.
 type Context struct {
@@ -16,9 +26,11 @@ type Context struct {
 
 // NewContext builds a new Z3 environment with the given configuration.
 func NewContext(cfg *Config) *Context {
-	return &Context{
+	c := &Context{
 		ctx: C.Z3_mk_context(cfg.cfg),
 	}
+	C.Z3_set_error_handler(c.ctx, (*C.Z3_error_handler)(C.errorhandler))
+	return c
 }
 
 // Close frees memory associated with this context.
