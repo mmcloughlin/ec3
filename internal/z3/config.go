d604de9a862359a31c0db0fbedeeb94575293163
Michael McLoughlin <mmcloughlin@gmail.com>
2019-10-19 00:59:02 -0700

    internal/z3: allow setting config options
    Updates #99 #101
    
    [ci skip]

--- internal/z3/config.go
+++ internal/z3/config.go
@@ -3,8 +3,15 @@ package z3
 /*
 #cgo LDFLAGS: -lz3
 #include <z3.h>
+#include <stdlib.h>
 */
 import "C"
+
+import (
+	"strconv"
+	"time"
+	"unsafe"
+)
 
 // Config contains configuration options.
 type Config struct {
@@ -22,4 +29,47 @@ func NewConfig() *Config {
 func (c *Config) Close() error {
 	C.Z3_del_config(c.cfg)
 	return nil
+}
+
+// SetParam sets a configuration parameter.
+func (c *Config) SetParam(id, value string) {
+	cid := C.CString(id)
+	cvalue := C.CString(value)
+
+	defer C.free(unsafe.Pointer(cid))
+	defer C.free(unsafe.Pointer(cvalue))
+
+	C.Z3_set_param_value(c.cfg, cid, cvalue)
+}
+
+// SetParamBool sets a boolean parameter.
+func (c *Config) SetParamBool(id string, value bool) {
+	c.SetParam(id, strconv.FormatBool(value))
+}
+
+// SetProof configures proof generation.
+func (c *Config) SetProof(enabled bool) {
+	c.SetParamBool("proof", enabled)
+}
+
+// SetTimeout configures the default timeout used for solvers.
+func (c *Config) SetTimeout(d time.Duration) {
+	c.SetParam("timeout", strconv.FormatInt(d.Milliseconds(), 10))
+}
+
+// SetAutoConfig configures whether to use heuristics to automatically select
+// solver and configure it.
+func (c *Config) SetAutoConfig(enabled bool) {
+	c.SetParamBool("auto_config", enabled)
+}
+
+// SetModel configures model generation for solvers. This parameter can be
+// overwritten when creating a solver.
+func (c *Config) SetModel(enabled bool) {
+	c.SetParamBool("model", enabled)
+}
+
+// SetModelValidate configures whether to validate models produced by solvers.
+func (c *Config) SetModelValidate(enabled bool) {
+	c.SetParamBool("model_validate", enabled)
 }
