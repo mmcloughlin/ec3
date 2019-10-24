6cb3b13b4d61218bfa45995359ae623468b42cfd
Michael McLoughlin <mmcloughlin@gmail.com>
2019-10-23 22:11:39 -0400

    arith/ir: printing for arith programs

--- arith/ir/ir.go
+++ arith/ir/ir.go
@@ -1,13 +1,23 @@
 // Package ir defines an intermediate representation for multi-precision arithmetic.
 package ir
 
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
 // Operand is an interface for instruction operands.
 type Operand interface {
+	fmt.Stringer
+
 	operand()
 }
 
 // Register is a machine word operand.
 type Register string
+
+func (r Register) String() string { return string(r) }
 
 func (Register) operand() {}
 
@@ -25,6 +35,8 @@ func SelectRegisters(ops []Operand) []Register {
 // Constant is a constant operand.
 type Constant uint64
 
+func (c Constant) String() string { return fmt.Sprintf("$%#x", uint64(c)) }
+
 func (Constant) operand() {}
 
 // Zero is the zero constant.
@@ -33,6 +45,8 @@ var Zero = Constant(0)
 // Flag is a single-bit constant operand.
 type Flag uint64
 
+func (f Flag) String() string { return fmt.Sprintf("$%d", uint64(f)) }
+
 func (Flag) operand() {}
 
 // Program is a sequence of instructions.
@@ -40,11 +54,29 @@ type Program struct {
 	Instructions []Instruction
 }
 
+func (p *Program) String() string {
+	s := ""
+	for _, i := range p.Instructions {
+		s += FormatInstruction(i) + "\n"
+	}
+	return s
+}
+
 // Instruction in the intermediate representation.
 type Instruction interface {
 	Operands() []Operand
 
 	instruction()
+}
+
+// FormatInstruction returns a string representation of the instruction.
+func FormatInstruction(i Instruction) string {
+	mneumonic := reflect.TypeOf(i).Name()
+	ops := []string{}
+	for _, op := range i.Operands() {
+		ops = append(ops, op.String())
+	}
+	return mneumonic + "\t" + strings.Join(ops, ", ")
 }
 
 // MOV is a move instruction.
