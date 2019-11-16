663238190853c090e29b3c1e338cdcf6d3aa2bd3
Michael McLoughlin <mmcloughlin@gmail.com>
2019-11-16 13:39:17 -0800

    z3: add smt2 benchmark output

--- z3/solver.go
+++ z3/solver.go
@@ -3,12 +3,14 @@ package z3
 /*
 #cgo LDFLAGS: -lz3
 #include <z3.h>
+#include <stdlib.h>
 */
 import "C"
 
 import (
 	"errors"
 	"log"
+	"unsafe"
 )
 
 // Solver checks a collection of predicates for satisfiability.
@@ -74,7 +76,8 @@ func (s *Solver) Assert(c *Bool) {
 
 // Check whether the assertions in a given solver are consistent or not.
 func (s *Solver) Check() (bool, error) {
-	log.Printf("solver:\n%s", s.String())
+	log.Printf("solver:\n%s", s.SMT2("mul", "QF_BV", "unsat", ""))
+
 	res := C.Z3_solver_check(s.ctx, s.solver)
 	if res == C.Z3_L_UNDEF {
 		reason := C.Z3_solver_get_reason_unknown(s.ctx, s.solver)
@@ -96,4 +99,31 @@ func (s *Solver) Prove(f *Bool) (bool, error) {
 // Help returns a string describing all available solver parameters.
 func (s *Solver) Help() string {
 	return C.GoString(C.Z3_solver_get_help(s.ctx, s.solver))
+}
+
+// SMT2 converts the benchmark to SMT-LIB formatted string.
+func (s *Solver) SMT2(name, logic, status, attr string) string {
+	astvec := C.Z3_solver_get_assertions(s.ctx, s.solver)
+	n := uint(C.Z3_ast_vector_size(s.ctx, astvec))
+	assertions := make([]C.Z3_ast, n)
+	for i := uint(0); i < n; i++ {
+		assertions[i] = C.Z3_ast_vector_get(s.ctx, astvec, C.unsigned(i))
+	}
+
+	cstrs := []*C.char{}
+	for _, s := range []string{name, logic, status, attr} {
+		cstr := C.CString(s)
+		cstrs = append(cstrs, cstr)
+		defer C.free(unsafe.Pointer(cstr))
+	}
+
+	return C.GoString(C.Z3_benchmark_to_smtlib_string(
+		s.ctx,
+		cstrs[0],
+		cstrs[1],
+		cstrs[2],
+		cstrs[3],
+		C.unsigned(n-1),
+		&assertions[0],
+		assertions[n-1]))
 }
