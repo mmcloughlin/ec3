f29e8557742d7a7234feb4a3d7aa56b71f452af3
Michael McLoughlin <mmcloughlin@gmail.com>
2020-06-19 22:20:48 -0700

    tests

--- arith/fp/mont/mont_test.go
+++ arith/fp/mont/mont_test.go
@@ -1,97 +1,127 @@
 package mont
 
 import (
+	"math/big"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/mmcloughlin/ec3/arith/build"
-	"github.com/mmcloughlin/ec3/arith/verif"
-	"github.com/mmcloughlin/ec3/internal/assert"
+	"github.com/mmcloughlin/ec3/arith/eval/m64"
+	"github.com/mmcloughlin/ec3/internal/bigint"
+	"github.com/mmcloughlin/ec3/internal/test"
 	"github.com/mmcloughlin/ec3/prime"
-	"github.com/mmcloughlin/ec3/z3"
 )
 
 func TestAdd(t *testing.T) {
-	// Initialize context.
-	cfg := z3.NewConfig()
-	defer cfg.Close()
-	ctx := z3.NewContext(cfg)
-	defer ctx.Close()
-
 	p := prime.NISTP256
-	spec, err := verif.AddMod(ctx, 64, 4, p.Int())
-	assert.NoError(t, err)
+	field := New(p)
+	k := field.Limbs()
 
-	f := New(p)
+	// Build program.
+	ctx := build.NewContext()
+	X := ctx.Int("X", k)
+	Y := ctx.Int("Y", k)
+	Z := ctx.Int("Z", k)
 
-	x, err := spec.Registers("x")
-	assert.NoError(t, err)
-	y, err := spec.Registers("y")
-	assert.NoError(t, err)
-	z, err := spec.Registers("z")
-	assert.NoError(t, err)
+	field.Add(ctx, Z, X, Y)
 
-	bld := build.NewContext()
-	f.Add(bld, z, x, y)
-
-	prog, err := bld.Program()
-	assert.NoError(t, err)
-	t.Logf("program:\n%s", prog)
-
-	result, err := spec.Prove(prog)
+	prog, err := ctx.Program()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if !result {
-		assignments, err := spec.Counterexample()
-		assert.NoError(t, err)
-		for name, x := range assignments {
-			t.Logf("%s = %#x", name, x)
+	t.Logf("program:\n%s", prog)
+
+	// got: use the evaluator
+	f := func(x, y *big.Int) *big.Int {
+		e := m64.NewEvaluator()
+		e.SetInt(X, x)
+		e.SetInt(Y, y)
+		if err := e.Execute(prog); err != nil {
+			t.Fatal(err)
 		}
-		t.Fatal("expected true")
+		z, err := e.Int(Z)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return z
 	}
+
+	// expect: modular addition function on two big integers
+	g := func(x, y *big.Int) *big.Int {
+		z := new(big.Int).Add(x, y)
+		z.Mod(z, p.Int())
+		return z
+	}
+
+	// Random trials.
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	test.Repeat(t, func(t *testing.T) bool {
+		x := new(big.Int).Rand(r, p.Int())
+		y := new(big.Int).Rand(r, p.Int())
+		got := f(x, y)
+		expect := g(x, y)
+		if !bigint.Equal(expect, got) {
+			t.Fail()
+		}
+		return true
+	})
 }
 
 func TestSub(t *testing.T) {
-	// TODO(mbm): reduce duplication from the addition test
-
-	// Initialize context.
-	cfg := z3.NewConfig()
-	defer cfg.Close()
-	ctx := z3.NewContext(cfg)
-	defer ctx.Close()
-
 	p := prime.NISTP256
-	spec, err := verif.SubMod(ctx, 64, 4, p.Int())
-	assert.NoError(t, err)
+	field := New(p)
+	k := field.Limbs()
 
-	f := New(p)
+	// Build program.
+	ctx := build.NewContext()
+	X := ctx.Int("X", k)
+	Y := ctx.Int("Y", k)
+	Z := ctx.Int("Z", k)
 
-	x, err := spec.Registers("x")
-	assert.NoError(t, err)
-	y, err := spec.Registers("y")
-	assert.NoError(t, err)
-	z, err := spec.Registers("z")
-	assert.NoError(t, err)
+	field.Sub(ctx, Z, X, Y)
 
-	bld := build.NewContext()
-	f.Sub(bld, z, x, y)
-
-	prog, err := bld.Program()
-	assert.NoError(t, err)
-	t.Logf("program:\n%s", prog)
-
-	result, err := spec.Prove(prog)
+	prog, err := ctx.Program()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if !result {
-		assignments, err := spec.Counterexample()
-		assert.NoError(t, err)
-		for name, x := range assignments {
-			t.Logf("%s = %#x", name, x)
+	t.Logf("program:\n%s", prog)
+
+	// got: use the evaluator
+	f := func(x, y *big.Int) *big.Int {
+		e := m64.NewEvaluator()
+		e.SetInt(X, x)
+		e.SetInt(Y, y)
+		if err := e.Execute(prog); err != nil {
+			t.Fatal(err)
 		}
-		t.Fatal("expected true")
+		z, err := e.Int(Z)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return z
 	}
+
+	// expect: modular subtraction function on two big integers
+	g := func(x, y *big.Int) *big.Int {
+		z := new(big.Int).Sub(x, y)
+		z.Add(z, p.Int())
+		z.Mod(z, p.Int())
+		return z
+	}
+
+	// Random trials.
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	test.Repeat(t, func(t *testing.T) bool {
+		x := new(big.Int).Rand(r, p.Int())
+		y := new(big.Int).Rand(r, p.Int())
+		got := f(x, y)
+		expect := g(x, y)
+		if !bigint.Equal(expect, got) {
+			t.Fail()
+		}
+		return true
+	})
 }
